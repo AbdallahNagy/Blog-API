a89e185c1ab235a08acdfc12c304ec9e37ae72a0
abdallah nagy <abdallahnagy773@gmail.com>
2024-03-06 23:42:47 +0200

    refactor tags manager by using Mapster

--- BL.Blog/Managers/Tags/TagManager.cs
+++ BL.Blog/Managers/Tags/TagManager.cs
@@ -1,21 +1,19 @@
 ﻿using Blog.BL.DTOs.Tags;
 using Blog.BL.Exception_Handling;
 using Blog.DAL.Repos.Tags;
+using Mapster;
 
 namespace Blog.BL.Managers.Tags;
 
-public class TagManager : ITagManager
+public class TagManager(ITagRepo tagRepo) : ITagManager
 {
-    private readonly ITagRepo _tagRepo;
-    public TagManager(ITagRepo tagRepo)
-    {
-        _tagRepo = tagRepo;
-    }
+    private readonly ITagRepo _tagRepo = tagRepo;
+
     public async Task<IEnumerable<ReadTagDTO>?> GetAll()
     {
         var tags = await _tagRepo.GetAll()
             ?? throw new BusinessException(404, "Tags not found");
 
-        return tags.Select(t => new ReadTagDTO(t.Id, t.Name, t.CreatedAt));
+        return tags.Adapt<IEnumerable<ReadTagDTO>>();
     }
 }
